fbb189c915bd5a9ba91de9e8102c8dc3e8c90d70
Todd Albert <todd@bocacode.com>
2022-03-01 14:47:19 -0500

    using JWT for getUsers

--- src/scenes/Login.js
+++ src/scenes/Login.js
@@ -1,16 +1,20 @@
 import { useState } from 'react'
+import bcrypt from 'bcryptjs'
+
+const salt = '$2b$10$A7/SX9iqJkE7jUs0vi38Gu'
 
 export default function Login({ setToken, setIsUser }) {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const handleSubmit = (e) => {
     e.preventDefault()
+    const hashedPassword = bcrypt.hashSync(password, salt)
     fetch('http://localhost:3001/users/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ email, password })
+      body: JSON.stringify({ email, password: hashedPassword })
     })
       .then(response => response.json())
       .then(data => setToken(data.token))
